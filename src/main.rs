c85111718e38b32795d42b1b0fe714ad41363d90
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2022-08-08 12:28:07 +1000

    Switched to eframe/egui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
-extern crate conrod_core;
+extern crate eframe;
+
+use eframe::egui;
 
 fn main() {
-    
+    let options = eframe::NativeOptions::default(); // Set window options
+    eframe::run_native(
+    "Todo", // Name
+    options, // Options
+    Box::new(|cc| Box::new(TodoApp::new(cc)))); // App hand over
+}
+
+// App definition
+#[derive(Default)]
+struct TodoApp {}
+
+impl TodoApp {
+    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default() // Return
+    }
+}
+
+impl eframe::App for TodoApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
 }
