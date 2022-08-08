3a828535e39309d5602512872d3dc1e1825552f2
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2022-08-08 18:19:32 +1000

    Dynamic button adding test

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,35 @@ fn main() {
 
 // App definition
 #[derive(Default)]
-struct TodoApp {}
+struct TodoApp {
+    buttons: Vec<String>,
+}
 
 impl TodoApp {
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        Self::default() // Return
+        let mut me = Self::default(); // Return
+        me.add_button(); // Push first button
+        me
+    }
+
+    fn add_button(&mut self) {
+        self.buttons.push("Button".to_string());
     }
 }
 
+// Update impl
 impl eframe::App for TodoApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello World!");
+            ctx.set_visuals(egui::Visuals::dark()); // Set visuals to dark mode
+
+            ui.heading("Hello World!"); // Hello world text
+
+            for i in 0..self.buttons.len() {
+                if ui.button(self.buttons[i].as_str()).clicked() {
+                    self.add_button(); // On click, add button
+                }
+            }
         });
     }
 }
